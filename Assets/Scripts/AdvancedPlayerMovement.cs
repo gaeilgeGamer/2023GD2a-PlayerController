73d9d44b48325fae779c50408b71aaed1bad01b7
naoise collins <naoisecollins@gmail.com>
2023-10-05 15:29:34 +0100

    add old functions for movement

--- Assets/Scripts/AdvancedPlayerMovement.cs
+++ Assets/Scripts/AdvancedPlayerMovement.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class AdvancedPlayerMovement : MonoBehaviour
 {
+    public float speed = 10f;
+    public float jumpHeight = 7f; 
+    public float dashSpeed = 20f;
+    public float crouchHeight = 0.5f;
+    public LayerMask whatIsGround; 
+    public Transform groundCheckPoint; 
+    public float groundCheckRadius = 0.2f;
+
+    public AudioClip jumpSound; 
+    public AudioClip dashSound; 
+    public AudioClip footstepSound; 
+
+    private Rigidbody2D body; 
+    private Animator anim; 
+    private AudioSource audioSource; 
+    private bool grounded; 
+    private bool canDoubleJump = false;
+    private bool isDashing = false; 
+    private bool isCrouching = false;
+    private bool facingRight = true; 
+
+
+    
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        grounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
         
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        body.velocity = new Vector2(horizontalInput*speed, body.velocity.y);
+        anim.SetBool("walk", horizontalInput !=0);
+    }
+    private void Flip(){
+        Vector3 currentScale = gameObject.transform.localScale; 
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale; 
+        facingRight = !facingRight; 
+    }
+    private void Jump(){
+        body.velocity = new Vector2(body.velocity.x, jumpHeight);
+        anim.SetTrigger("jump");
+        grounded = false;
     }
 }
