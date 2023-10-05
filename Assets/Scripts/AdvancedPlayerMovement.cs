f6d5ff7807d851033d4c3c76c47316895bb656fa
naoise collins <naoisecollins@gmail.com>
2023-10-05 15:57:17 +0100

    Add crouching mechanic

--- Assets/Scripts/AdvancedPlayerMovement.cs
+++ Assets/Scripts/AdvancedPlayerMovement.cs
@@ -19,8 +19,8 @@ public class AdvancedPlayerMovement : MonoBehaviour
     private Rigidbody2D body; 
     private Animator anim; 
     private AudioSource audioSource; 
-    private bool grounded; 
-    private bool canDoubleJump = false;
+    public bool grounded; 
+    public bool canDoubleJump = false;
     private bool isDashing = false; 
     private bool isCrouching = false;
     private bool facingRight = true; 
@@ -44,14 +44,36 @@ public class AdvancedPlayerMovement : MonoBehaviour
         body.velocity = new Vector2(horizontalInput*speed, body.velocity.y);
         anim.SetBool("walk", horizontalInput !=0);
 
+        if(Input.GetKeyDown(KeyCode.Space)&& grounded)
+        {
+            canDoubleJump = true; 
+            Jump();
+             
+        }
+        else if(Input.GetKeyDown(KeyCode.Space)&&canDoubleJump)
+        {
+            Jump();
+            canDoubleJump = false;
+        }
+
         if((horizontalInput>0&& !facingRight)||(horizontalInput<0&&facingRight))
         {
             Flip();
         }
-        if(Input.GetKey(KeyCode.Space)&& grounded)
+        if (Input.GetKeyDown(KeyCode.DownArrow)&& grounded)
         {
-            Jump();
+            if(!isCrouching){
+                transform.localScale = new Vector3(transform.localScale.x, crouchHeight, transform.localScale.z);
+                isCrouching = true;
+            }
+             else if (isCrouching)
+        {
+            transform.localScale = new Vector3(transform.localScale.x,1f,transform.localScale.z);
+            isCrouching = false; 
         }
+        }
+
+        
     }
     private void Flip(){
         Vector3 currentScale = gameObject.transform.localScale; 
