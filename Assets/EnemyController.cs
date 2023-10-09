1eb95ffd7600b586858b46f4d4c32d35e491facc
naoise collins <naoisecollins@gmail.com>
2023-10-09 15:33:09 +0100

    Add EnemySpawner, prefab and Move Conditions

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float groundCheckDistance = .6f;
+    [SerializeField] private LayerMask whatIsGround; 
+    private bool movingRight = true; 
+
+    [Header("Combat Settings")]
+    [SerializeField] private int maxHealth = 5;
+    private int currentHealth;
+    [SerializeField] private int damage = 1; 
+
+    private Rigidbody2D enemyRigidBody; 
+    private EnemySpawner spawner;  
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        enemyRigidBody = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+    }
+
+    public void Initialize(EnemySpawner spawnerReference)
+    {
+       spawner = spawnerReference;
+  }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Move(){
+        Vector2 groundCheckPosition = movingRight ?
+            new Vector2(transform.position.x + 0.5f, transform.position.y):
+            new Vector2(transform.position.x - 0.5f, transform.position.y);
+
+            bool isGrounded = Physics2D.Raycast(groundCheckPosition, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+}
---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [Header("Spawn Settings")]
+    [SerializeField] private GameObject enemyPrefab; 
+    [SerializeField] private float spawnRate = 2f; 
+    [SerializeField] private Transform[] spawnPoints; 
+
+    [Header("Limit Settings")]
+    [SerializeField] private int maxEnemies = 5; 
+    private int currentEnemies = 0; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void SpawnEnemy(){
+        if(currentEnemies > maxEnemies) return; 
+
+        Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        GameObject spawnedEnemy = Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity);
+
+        EnemyController enemyController = spawnedEnemy.GetComponent<EnemyController>();
+        enemyController.Initialize(this);
+
+        currentEnemies++;
+    }
+    public void EnemyDied(){
+        currentEnemies--;
+    }
+}
